f6059adea9f749313f2182cb81487d4cefbf89e6
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-29 21:16:57 -0300

    Feat: prospect removal

--- src/components/remove-prospect-modal.tsx
+++ src/components/remove-prospect-modal.tsx
@@ -6,8 +6,49 @@ import {
     DialogHeader,
     DialogTitle,
 } from './ui/dialog'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'react-toastify'
+import { api } from '@/lib/axios'
 
-export function RemoveProspectModal() {
+interface RemoveProspectModalProps {
+    document: string
+    onClose: () => void
+}
+
+export function RemoveProspectModal({
+                                        document,
+                                        onClose,
+                                    }: RemoveProspectModalProps) {
+    const queryClient = useQueryClient()
+
+    const deleteProspect = useMutation({
+        mutationFn: () => {
+            return api.delete(
+                `delete/${document}/${document.length === 14 ? 'pj' : 'pf'}`,
+            )
+        },
+        onSuccess: () => {
+            toast.success('Removido.', {
+                position: 'bottom-right',
+                theme: 'colored',
+                autoClose: 3000,
+            })
+            queryClient.invalidateQueries(['prospects'])
+        },
+        onError: () => {
+            toast.error('Houve um problema.', {
+                position: 'bottom-right',
+                theme: 'colored',
+                autoClose: 3000,
+            })
+        },
+    })
+
+    const handleDelete = () => {
+        deleteProspect.mutate()
+        onClose()
+    }
+
     return (
         <DialogContent className="bg-white max-w-sm flex flex-col">
             <DialogHeader>
@@ -22,10 +63,16 @@ export function RemoveProspectModal() {
                     type="button"
                     className="text-primary hover:text-primary rounded"
                     variant={'outline'}
+                    onClick={() => onClose()}
                 >
                     Cancelar
                 </Button>
-                <Button type="button" variant="destructive" className="rounded">
+                <Button
+                    type="button"
+                    variant="destructive"
+                    className="rounded"
+                    onClick={handleDelete}
+                >
                     Excluir
                 </Button>
             </div>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -3,24 +3,53 @@ import { RemoveProspectModal } from '@/components/remove-prospect-modal'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { useProspects } from '@/hooks/queries/prospects'
+import { api } from '@/lib/axios'
 import { Dialog, DialogTrigger } from '@radix-ui/react-dialog'
 import { Separator } from '@radix-ui/react-separator'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { useMediaQuery } from '@uidotdev/usehooks'
 import { FileText, PlusIcon, Save, Edit2, Trash2 } from 'lucide-react'
+import { useState } from 'react'
 import { toast } from 'react-toastify'
-// import {zodResolver} from "@hookform/resolvers/zod";
-export function Dashboard() {
-    //Multidispositivo
-    const isSmallDevice = useMediaQuery('only screen and (max-width : 768px)')
-    const { data: prospects } = useProspects()
 
-    console.log('DATA', prospects)
+export function Dashboard() {
+    const isSmallDevice = useMediaQuery('only screen and (max-width : 768px)')
+    const [openAddModal, setOpenAddModal] = useState<boolean>(false)
+    const [openEditModal, setOpenEditModal] = useState<boolean>(false)
+    const [openDeleteModal, setOpenDeleteModal] = useState<boolean>(false)
+    const { data: prospects } = useProspects()
+    const queryClient = useQueryClient()
+
+    const consumeProspect = useMutation({
+        mutationFn: () => {
+            return api.get('consume-prospect')
+        },
+        onSuccess: () => {
+            toast.success('Processado.', {
+                position: 'bottom-right',
+                theme: 'colored',
+                autoClose: 3000,
+            })
+            queryClient.invalidateQueries(['prospects'])
+        },
+        onError: () => {
+            toast.error('Houve um problema.', {
+                position: 'bottom-right',
+                theme: 'colored',
+                autoClose: 3000,
+            })
+        },
+    })
 
     const handleProcess = () => {
-        toast.info('A fila está vazia.', {
-            position: 'bottom-right',
-            theme: 'colored',
-        })
+        consumeProspect.mutate()
+
+        if (prospects?.length === 0) {
+            toast.info('A fila está vazia.', {
+                position: 'bottom-right',
+                theme: 'colored',
+            })
+        }
     }
 
     return (
@@ -34,17 +63,18 @@ export function Dashboard() {
                         </h2>
                     </div>
                     <div className="flex gap-2">
-                        <Dialog>
+                        <Dialog open={openAddModal}>
                             <DialogTrigger>
                                 <Button
                                     size={isSmallDevice ? 'icon' : 'default'}
                                     className="rounded"
+                                    onClick={() => setOpenAddModal(true)}
                                 >
                                     <span className="hidden md:block">Adicionar</span>
                                     <PlusIcon className="block md:hidden" />
                                 </Button>
                             </DialogTrigger>
-                            <ProspectForm />
+                            <ProspectForm onClose={() => setOpenAddModal(false)} />
                         </Dialog>
                         <Button
                             size={isSmallDevice ? 'icon' : 'default'}
@@ -73,42 +103,64 @@ export function Dashboard() {
                             </tr>
                             </thead>
                             <tbody>
-                            {prospects?.map((prospect) => (
-                                <tr
-                                    key={prospect.uuid}
-                                    className="text-sm md:text-xs lg:text-[10px] text-[#54646E] whitespace-nowrap"
-                                >
-                                    <td className="border px-4 py-2">{prospect.cnpj || '-'}</td>
-                                    <td className="border px-4 py-2">
-                                        {prospect.corporateName || '-'}
-                                    </td>
-                                    <td className="border px-4 py-2">{prospect.mcc}</td>
-                                    <td className="border px-4 py-2">{prospect.cpf}</td>
-                                    <td className="border px-4 py-2">{prospect.name}</td>
-                                    <td className="border px-4 py-2">{prospect.email}</td>
-                                    <td className="flex justify-around border px-4 py-2">
-                                        <Button
-                                            variant="link"
-                                            size={'icon'}
-                                            className="text-[#54646E] hover:text-primary transition duration-300"
+                            {prospects &&
+                                prospects
+                                    .sort(
+                                        (a, b) =>
+                                            Date.parse(a.updatedAt) - Date.parse(b.updatedAt),
+                                    )
+                                    .map((prospect) => (
+                                        <tr
+                                            key={prospect.uuid}
+                                            className="text-sm md:text-xs lg:text-[10px] text-[#54646E] whitespace-nowrap"
                                         >
-                                            <Edit2 size="16" />
-                                        </Button>
-                                        <Dialog>
-                                            <DialogTrigger>
-                                                <Button
-                                                    variant="link"
-                                                    size={'icon'}
-                                                    className="text-[#54646E] hover:text-primary transition duration-300"
-                                                >
-                                                    <Trash2 size="16" />
-                                                </Button>
-                                            </DialogTrigger>
-                                            <RemoveProspectModal />
-                                        </Dialog>
-                                    </td>
-                                </tr>
-                            ))}
+                                            <td className="border px-4 py-2">
+                                                {prospect.cnpj || '-'}
+                                            </td>
+                                            <td className="border px-4 py-2">
+                                                {prospect.corporateName || '-'}
+                                            </td>
+                                            <td className="border px-4 py-2">{prospect.mcc}</td>
+                                            <td className="border px-4 py-2">{prospect.cpf}</td>
+                                            <td className="border px-4 py-2">{prospect.name}</td>
+                                            <td className="border px-4 py-2">{prospect.email}</td>
+                                            <td className="flex justify-around border px-4 py-2">
+                                                <Dialog open={openEditModal}>
+                                                    <DialogTrigger>
+                                                        <Button
+                                                            variant="link"
+                                                            size={'icon'}
+                                                            onClick={() => setOpenEditModal(true)}
+                                                            className="text-[#54646E] hover:text-primary transition duration-300"
+                                                        >
+                                                            <Edit2 size="16" />
+                                                        </Button>
+                                                    </DialogTrigger>
+                                                    <ProspectForm
+                                                        prospect={prospect}
+                                                        onClose={() => setOpenEditModal(false)}
+                                                    />
+                                                </Dialog>
+
+                                                <Dialog open={openDeleteModal}>
+                                                    <DialogTrigger>
+                                                        <Button
+                                                            variant="link"
+                                                            size={'icon'}
+                                                            className="text-[#54646E] hover:text-primary transition duration-300"
+                                                            onClick={() => setOpenDeleteModal(true)}
+                                                        >
+                                                            <Trash2 size="16" />
+                                                        </Button>
+                                                    </DialogTrigger>
+                                                    <RemoveProspectModal
+                                                        document={prospect.cnpj || prospect.cpf}
+                                                        onClose={() => setOpenDeleteModal(false)}
+                                                    />
+                                                </Dialog>
+                                            </td>
+                                        </tr>
+                                    ))}
                             </tbody>
                         </table>
                     </div>
